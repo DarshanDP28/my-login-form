fc29e81fbb5cd2262cfbcde16f60b1b6d453dbb8
darshan padmane <DARSHANPADMANEDP@GMAIL.COM>
2024-05-15 11:01:52 +0530

    login-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {useState}from "react";
 import './App.css';
+const App = ()=>{
+    const[ username,setUsername ] = useState(' '); //hooks
+    const[ password,setPassword ] = useState(' ');
+    const[ showPassword,setShowPassword ] = useState('true');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className="container">
+            <input
+            type="text"
+            placeholder="EnterUsername"
+            value={username}
+            onChange={(e) =>setUsername(e.target.value)}
+            />
+            <input
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) =>setPassword(e.target.value)}
+            />
+            <h2>{username}</h2>
+            {/* if the showPassword is true then we display SHOW else we display NOT SHOW  */}
+            <h2>{showPassword ? password : '*****'}</h2>
+            <button onClick = {(e) => setShowPassword(!showPassword)}>Show/Hide password</button>
+
+        </div>
+    )
 }
 
 export default App;
